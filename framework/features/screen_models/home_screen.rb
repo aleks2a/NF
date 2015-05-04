deec745f97e5f65615268d973bad8fdcfc3bf00b
Aliaksei Piatrouski <Aliaksei@Piatrouski>
2015-05-04 16:52:49 -0700

    trigger tests

--- framework/features/screen_models/home_screen.rb
+++ framework/features/screen_models/home_screen.rb
@@ -1,11 +1,13 @@
 class HomeScreen < PageActions
 
+  element :sign_in, css: "span.text"
+
   def get_home
     driver.get "http://www.netflix.com"
   end
 
-  def sign_in
-    find_element(:css, "span.text")
-  end
+  # def sign_in
+  #   find_element(:css, "span.text")
+  # end
 
-end 
+end
